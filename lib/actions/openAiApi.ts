2fbf2d26564a79f6ca198720792a2755594add6c
Jakub Mazur <j.mazur.nl@gmail.com>
2024-10-04 15:00:16 +0200

    🔧 Make small scores 0 so we don't display really small bars

--- lib/actions/openAiApi.ts
+++ lib/actions/openAiApi.ts
@@ -14,11 +14,11 @@ export default async function fetchApiScores(inputText: string) {
 
   const result = moderation.results[0]; // Extract the first result (assuming single input)
 
-  // Filter out subcategories (categories that contain a "/")
+  // Filter out subcategories (categories that contain a "/") and set scores below 0.05 to 0
   const filteredScores = Object.fromEntries(
-    Object.entries(result.category_scores).filter(
-      ([category]) => !category.includes("/"),
-    ),
+    Object.entries(result.category_scores)
+      .filter(([category]) => !category.includes("/"))
+      .map(([category, score]) => [category, score < 0.05 ? 0 : score]),
   );
 
   // Return the flagged status and filtered category scores
